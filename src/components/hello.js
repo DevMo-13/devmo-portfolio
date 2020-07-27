1f8a5f46dd9115b79ed342c901ddc3d1b1d3a972
DevMo-13 <moniquecinatl@gmail.com>
2020-07-27 10:24:07 -0700

    Updated project data

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,12 +3,10 @@ import React from 'react';
 export default function Hello() {
 	return (
 		<section className='hello section'>
-			<div>
-				<img alt='bridge' src='bridge.jpg' width='100%'/>
-				<h1>MONIQUE RICO CINATL</h1>
-				<hr/>
-				<h2>Developer. Creator. Collaborator.</h2>
-			</div>
+			<img alt='bridge' src='bridge.jpg' width='100%'/>
+			<h1>MONIQUE RICO CINATL</h1>
+			<hr/>
+			<h2>Developer. Creator. Collaborator.</h2>
 		</section>
 	)
 }
