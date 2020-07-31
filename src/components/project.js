8826e048cc863d09bbbb2c5a103b3e2c922d66d2
DevMo-13 <moniquecinatl@gmail.com>
2020-07-31 16:08:43 -0700

    Added projects sesction and other minor updates

---
+++ src/components/project.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './css/projects.module.css';
+
+export default function Project({ id, name }) {
+
+	return (
+		<div className={styles.projDiv}>
+			<li>
+				<h3 className={styles.h3}>{name}</h3>
+				<div className={styles.imgDiv}>
+					<img className={styles.img} src='pattern.jpeg'/>
+				</div>
+			</li>
+		</div>
+	)
+}
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,42 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import Project from './project'
+import styles from './css/projects.module.css';
 
 export default function Projects() {
-	
+	const projectsData = useStaticQuery(graphql`
+    	query ProjectsData {
+            allDataJson {
+                edges {
+                    node {
+                        projects {
+							id
+							projName
+						}
+					}
+				}
+			}
+		}
+	`);
+
+	const projectsArray = projectsData.allDataJson.edges[0].node.projects;
+	console.log(projectsArray);
+
+	const projects = projectsArray.map(project => {
+		return (
+			<Project
+				key={project.id}
+				id={project.id}
+				name={project.projName}
+			/>
+		)
+	});
+
 	return (
-		<section className='projects-section'>
-			<div>
-				<h3>NEED TO ADD PROJECTS HERE</h3>
-			</div>
+		<section className={styles.projectsSection}>
+			<ul className={styles.ul}>
+				{projects}
+			</ul>
 		</section>
 	)
 }
