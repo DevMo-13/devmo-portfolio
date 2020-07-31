d5f51c07d5e00d0df2fcaa5186266f9671ff1492
DevMo-13 <moniquecinatl@gmail.com>
2020-07-30 20:04:33 -0700

    Made hello, about, and footer sections responsive

--- src/components/about.js
+++ src/components/about.js
@@ -4,15 +4,18 @@ import styles from './css/about.module.css'
 // Renders the About section of the page.
 export default function About() {
 	return (
-		<section className='about section' className={`${styles.sectionPadding} ${styles.backgroundColor}`}>
+		<section className={styles.aboutSection}>
 			<div>
 				<img className={styles.img} alt='headshot' src='profile-pic3.jpg'/>
 			</div>
 			<div>
 				<h2 className={styles.h2}>Hi there! I'm Monique.</h2>
-				<p className={`${styles.pPadding} ${styles.p}`}>I am a full stack web developer with 7+ years of experience working in higher education. I am eager to bring my strong organization and communication skills, the ability to effectively problem solve and troubleshoot issues, and a demonstrated drive to succeed into a new career in tech.
-				<br/> <br/>
-				I am a recent Treehouse Full Stack JavaScript techdegree grad, and I am looking forward to a lifetime of continued learning. Take a look at the projects below to see what I built throughout my techdegree journey. As a code newbie, I am eager to learn and improve, so if you have any feedback you could provide or know of any job opportunities, I would love to get in touch!</p>
+				<p className={styles.p}>I am a full stack web developer with 7+ years 
+				of experience working in higher education. I am eager to bring my strong organization and 
+				communication skills, the ability to effectively problem solve and troubleshoot issues, and a 
+				demonstrated drive to succeed into a new career in tech.<br/><br/>I am a recent Treehouse Full 
+				Stack JavaScript techdegree grad, and I am looking forward to a lifetime of continued learning. 
+				Take a look at the projects below to see what I built throughout my techdegree journey.</p>
 			</div>
 		</section>
 	)
--- src/components/hello.js
+++ src/components/hello.js
@@ -4,7 +4,7 @@ import styles from './css/hello.module.css'
 // Renders the Hello section of the page.
 export default function Hello() {
 	return (
-		<section className={styles.sectionPadding}>
+		<section className={styles.helloSection}>
 			<h1 className={styles.h1}>Monique Rico Cinatl</h1>
 			<h2 className={styles.h2}>Developer • Creator • Collaborator</h2>
 		</section>
