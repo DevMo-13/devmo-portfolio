2c3317d406f87bf57ca2b3b62533df83e9fff44e
DevMo-13 <moniquecinatl@gmail.com>
2020-08-08 14:13:15 -0700

    Started styling details page, and added prev / next project links

--- src/components/details.js
+++ src/components/details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import Footer from '../components/footer';
-// import styles from '../css/details.module.css';
+import styles from '../css/details.module.css';
 
 // Renders each project's details on its unique project-details page.
 export default function Details({ pageContext: { project} }) {
@@ -10,15 +10,23 @@ export default function Details({ pageContext: { project} }) {
 	const tech = technologies.map(function (technology, index) {
 		return <li key={index}>{technology}</li>;
 	});
+
+	// Turns project's id from a string into a number.
+	const projectId = parseInt(project.id);
 	
 	return (
 		<div className='content'>
 			<main>
-				<h1>{project.name}</h1>
+				<Link to='/'>Home</Link>
+				<Link to={`/project-details/${projectId - 1}`}>← Previous Project</Link>
+				<Link to={`/project-details/${projectId + 1}`}>Next Project →</Link>
+				<h1 className={styles.h1}>{project.name}</h1>
 
-				<p>{project.description}</p>
+				<p className={styles.desc}>{project.description}</p>
 
-				<p>{project.githubLink}</p>
+				<p className={styles.github}>
+					<a href={project.githubLink}>Source Code</a>
+				</p>
 
 				<ul>
 					{tech}
@@ -28,10 +36,14 @@ export default function Details({ pageContext: { project} }) {
 				<img src={project.otherImage1.path} alt={project.otherImage1.alt}/>
 				<img src={project.otherImage2.path} alt={project.otherImage2.alt}/>
 				<img src={project.otherImage3.path} alt={project.otherImage3.alt}/>
-
-				<Link to='/'>Home →</Link>
 			</main>
 			<Footer />
 		</div>
 	)
 }
+
+/*
+TO DO
+-fix prev and next buttons for when they reach the first/last projects
+- continue styling
+*/
