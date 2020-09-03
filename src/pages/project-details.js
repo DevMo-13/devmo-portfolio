7f161939a20692ead1e1d9dfb0db9965d611430c
DevMo-13 <moniquecinatl@gmail.com>
2020-09-02 19:20:30 -0700

    Updated CSS

---
+++ src/pages/project-details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Details from '../components/details';
+import Footer from '../components/footer';
+
+export default function ProjectDetails() {
+	return (
+		<main>
+			<Details />
+			<Footer />
+		</main>
+	)
+}
